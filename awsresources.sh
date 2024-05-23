38de4a716ac596bb327ae2df8dfcd12d27b744de
Ubuntu <ubuntu@ip-172-31-31-62.ec2.internal>
2024-05-23 06:20:01 +0000

    version 2 using crontab

--- awsresources.sh
+++ awsresources.sh
@@ -7,15 +7,18 @@
 #script will report the  aws resouce usuage
 ##########
 
-set -x
+touch sample.txt
+
+set -x 
 echo "list aws ec2 instances "
-aws ec2 describe-instances |jq '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances |jq '.Reservations[].Instances[].InstanceId' >>sample.txt
 
 echo "list s3 buckets "
-aws s3 ls
+aws s3 ls >>sample.txt
 
 echo " list lambda functions"
-aws lambda list-functions
+aws lambda list-functions >>sample.txt
 
 echo " list IAM users"
-aws iam list-users
+aws iam list-users >>sample.txt
+ 
